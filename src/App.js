8e438dd0bd1db3af93980f97b886150a702939d0
SaluKhadka <salu.khadka@machnetinc.com>
2021-01-22 17:45:59 +0545

    Add Todo compoments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import About from './components/layout/pages/About';
+import Header from './components/layout/Header';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    todos: [
+      {
+        id: uuidv4(),
+        title: 'Goal 1: Learn React',
+        completed: true
+      },
+      {
+        id: uuidv4(),
+        title: 'Goal 2: Level up knowledge in Java language',
+        completed: false
+      },
+      {
+        id: uuidv4(),
+        title: 'Get involved in reading habits',
+        completed: false
+      }
+    ]
+  }
+
+  toggleComplete = (id) => {
+    this.setState({
+      todo: this.state.todos
+        .filter(todo => todo.id === id)
+        .map(todo => todo.completed = !todo.completed)
+    });
+  }
+
+  deleteTodo = (id) => {
+    console.log(id);
+    this.setState({ todos: [...this.state.todos.filter(it => it.id !== id)] });
+  }
+
+  addTodo = (title) => {
+    console.log(title);
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <div className="container">
+            <Header />
+            <AddTodo addTodo= {this.addTodo} />
+            <Todos todos={ this.state.todos }
+       toggleComplete={this.toggleComplete}
+       deleteTodo={this.deleteTodo}/>
+                       </div>
+        </div>
+    );
+  }
 }
 
 export default App;
